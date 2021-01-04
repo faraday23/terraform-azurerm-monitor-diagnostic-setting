16d07df392e972215edf9e08f0dc98f854fc9db2
faraday23 <41235618+faraday23@users.noreply.github.com>
2021-01-04 09:11:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_monitor_diagnostic_setting" "diagsetting" {
       enabled  = true
 
       retention_policy {
-        enabled = lookup(var.ds_log_api_endpoints, log.value, false)
+        enabled = (map(var.ds_log_api_endpoints.value) == 0 ? false : true)
         days    = lookup(var.ds_log_api_endpoints, log.value, null)
       }
     }
@@ -28,7 +28,7 @@ resource "azurerm_monitor_diagnostic_setting" "diagsetting" {
       category = metric.value
 
       retention_policy {
-        enabled = lookup(var.ds_allmetrics_retention_days, metric.value, false)
+        enabled = (map(var.ds_allmetrics_retention_days.value) == 0 ? false : true)
         days    = lookup(var.ds_allmetrics_retention_days, metric.value, null)
       }
     }
