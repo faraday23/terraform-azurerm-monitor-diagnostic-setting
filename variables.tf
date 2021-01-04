9d855321c244e977d4337ba3f0073491b11419e2
faraday23 <41235618+faraday23@users.noreply.github.com>
2021-01-04 14:31:29 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -36,10 +36,10 @@ variable "metric_category" {
   default     = "AllMetrics"
 }
 
-variable "enable_retention_policy" {
-  type        = bool
-  description = "toggle on/off the retention policy for the metric"
-}
+#variable "enable_retention_policy" {
+#  type        = bool
+#  description = "toggle on/off the retention policy for the metric"
+#}
 
 
 
